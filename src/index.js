8fbd04289b2640035d1658f56119f3ddb8aaeb41
Tetiana Blinova <tiana002@gmail.com>
2023-06-08 20:21:58 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,68 +1,11 @@
-//  import React from 'react';
-//  import ReactDOM from 'react-dom/client';
- import { App } from 'components/App';
-// import './index.css';
+ import React from 'react';
+ import ReactDOM from 'react-dom/client';
+import { App } from './components/App';
+import './index.css';
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-
-
-class Counter extends Component {
-  // static defaultProps = {
-  //   step: 1,
-  //   initialValue: 0,
-  // };
-
-  // constructor(props) {
-  //   super(props);
-
-  //   this.state = {
-  //     value: this.props.initialValue,
-  //   };
-  // }
-  static defaultProps = {
-    step: 1,
-    initialValue: 0,
-  };
-
-  state = {
-    value: this.props.initialValue,
-  };
-
-
-  handleIncrement = () => {
-    this.setState((state, props) => ({
-      value: state.value + props.step,
-    })); 
-  };
-
-  handleDecrement = () => {
-    this.setState((state, props) => ({
-      value: state.value - props.step,
-    })); 
-  };
-
-  render() {
-    const { step } = this.props;
-
-    return (
-      <div>
-        <span>{this.state.value}</span>
-        <button type="button" onClick={this.handleIncrement}>
-          Increment by {step}
-        </button>
-        <button type="button" onClick={this.handleDecrement}>
-          Decrement by {step}
-        </button>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<Counter step={1} />, document.getElementById("root"));
